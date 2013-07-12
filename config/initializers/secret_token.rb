b210dbc1596793ce1a649a0c970ede00d237f63d
D Tucker <danielle@tuckerlabs.com>
2013-07-11 21:31:07 -0700

    Generate the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FavoriteLinks::Application.config.secret_token = '2ae83240dfdf02d738d939c30e8cb402268d5bd6774101fe90f7c345d505d18401cc87bda0c85412c8d3c60b7e8c5a845158260cd41775b45c5d8fbbde368128'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FavoriteLinks::Application.config.session_store :cookie_store, key: '_favorite_links_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FavoriteLinks::Application.config.session_store :active_record_store
